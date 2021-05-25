e519fd06a5588d6762ab041d71191edc87eb266b
Ignatio Julian <ignatio.julian@ti.ukdw.ac.id>
2021-05-25 11:12:46 +0800

    initial project file

---
+++ iOS-Spotify/iOS-Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Spotify
+//
+//  Created by Ignatio Julian on 25/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
